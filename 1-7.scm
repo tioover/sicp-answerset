606aa5eac8f809a0de33548acf634cb09dbdbd5e
tioover <tioover@gmail.com>
2013-06-27 21:42:36 +0800

    1.7 answer

---
+++ 1-7.scm
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (good-enough? guess x)
+  (<
+    (abs (- 1 (/ guess (improve guess x))))
+    0.0001
+  )
+)
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)
+  )
+)
+(sqrt-iter 1 2)
