7c48d839bf3264839bcd8430aa9655476da9c91c
soar tsui <tioover@gmail.com>
2013-06-27 21:54:26 +0800

    Update 1-6-test.scm

--- 1-6-test.scm
+++ 1-6-test.scm
@@ -1,6 +1,6 @@
 #lang racket
 
-(if #f (display "a") (display "b")) ; 结果打印b 可以看出，内置的if 是正则序正
+(if #f (display "a") (display "b")) ; 结果打印b 可以看出，内置的if 是正则序。
  
 (define (t a b) b)
-(t (display "a") (display "b")) ; 结果打印 ab 说明在代入参数过程中是需要进行求值的。
+(t (display "a") (display "b")) ; 结果打印 ab 说明在代入参数过程中是需要进行立即求值的。
