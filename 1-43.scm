46ffb23c684a1efbd8dbd725226c744dfbaa28cc
Tengfei Tsui <tioover@gmail.com>
2014-02-21 01:18:02 +0800

    Update 1-43.scm

--- 1-43.scm
+++ 1-43.scm
@@ -1,4 +1,4 @@
 #lang r5rs
 (define (square x) (* x x))
 (define (compose f g) (lambda (x) (f (g x))))
-(define (repeated f n) (if (= n 1) f (repeated (compose f f) (- n 1))))
+(define (repeated f n) (if (= n 1) f (compose f (repeated f (- n 1)))))
