d8aaff2f4c66f8f863616aad8af0bae3db24ae84
Tengfei Tsui <tioover@gmail.com>
2014-03-11 22:44:58 +0800

    Create 2-21.scm

---
+++ chapter-2/2-21.scm
@@ -0,0 +1,9 @@
+#lang r5rs
+(define nil `())
+(define (square x) (* x x))
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list (cdr items)))))
+(define (asquare-list items)
+  (map square items))
